67d4831e0f52683df439c66bb59edbe719efd472
alisson10000 <alisson_lima_souza@yahoo.com.br>
2023-02-26 14:10:01 -0300

    Update router.js

--- src/router.js
+++ src/router.js
@@ -12,6 +12,6 @@ routes.post('/Update', controllerProduto.Update);
 // routes.get('/GetOne', controllerProduto.GetOne);
 routes.post('/GetOne', controllerProduto.GetOne); // MUDAMOS PARA POST
 
-routes.post('/Delete', controllerProduto.Delete);
+routes.delete('/Delete', controllerProduto.Delete);
 
 module.exports = routes;
